de7989cc1d262e85b99b3dbfe94aab0307384378
alexplom <alexiosplomaritis@gmail.com>
2019-05-19 23:29:30 +0300

    Adds users component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CourseComponent } from './course/course.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
-  {path: 'courses', component: CourseComponent}
+  {path: 'courses', component: CourseComponent},
+  {path: 'users', component: UserComponent}
 ];
 
 @NgModule({
