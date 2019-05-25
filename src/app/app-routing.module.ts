af021d2d373fe22b070f9658c1b316e91961dd0e
alexplom <alexiosplomaritis@gmail.com>
2019-05-25 22:56:38 +0300

    I should have followed the angular tutorial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { CourseComponent } from './course/course.component';
 import { UserComponent } from './user/user.component';
 import { CourseDashboardComponent } from './course-dashboard/course-dashboard.component';
 import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: 'courses', component: CourseComponent},
-  { path: 'users', component: UserComponent},
-  { path: 'courses-admin', component: CourseDashboardComponent },
-  { path: 'detail/:id', component: CourseDetailComponent },
+  { path: 'users', canActivate: [AuthGuard], component: UserComponent},
+  { path: 'courses-admin', canActivate: [AuthGuard], component: CourseDashboardComponent },
+  { path: 'detail/:id', canActivate: [AuthGuard], component: CourseDetailComponent },
   { path: '', redirectTo: '/courses', pathMatch: 'full' },
   { path: '*', redirectTo: '/courses', pathMatch: 'full' }
 ];
@@ -18,4 +19,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService } from '../login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    
+      if(this.loginService.isUserAdmin()) { 
+      console.log("User is admin");
+      return true;
+    }
+    console.log("User is NOT!! admin");
+
+    return false;
+  }
+  constructor(private loginService: LoginService){
+
+  }
+  
+}
